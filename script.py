4120106e4c78e7a169004c2d4849327dd93c0a05
Michel Brevis <michel.brevis@gmail.com>
2025-02-05 00:03:21 -0300

    Update web scrappers

--- script.py
+++ script.py
@@ -1,8 +1,31 @@
-# Incorporate the necessary libraries
-import requests
-import bs4 import BeautifulSoup
+#Web scrapping wit Soup
 
-# Make a request to the website
+# 1. Incorporate the necessary libraries
+import requests
+from bs4 import BeautifulSoup
+
+# 2.Make a request to the website
 url = "https://www.portalinmobiliario.com/"
+
+# 3.Keep the respond from the website
+response = requests.get(url) # Get the HTML content of the website // request method 
+
+# 4. Verify the status code
+if response.status_code == 200:
+    print("The request was successful")#delete post verify the status code
+    soup = BeautifulSoup(response.text, "html.parser")
+    products = soup.find_all("div", class_="andes-carousel-snapped__slide andes-carousel-snapped__slide--next")
+    print(len(products))
+    
+    
+    for product in products:
+        title = product.find("span", class_="ui-item__bottom-title")
+        if title:
+            print(title.text.strip())
+            
+else:
+    print("The request failed with status code:", response.status_code)
+
+
 
 
